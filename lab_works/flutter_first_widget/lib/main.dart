902dc19cef5ae872023f98981da13eb0ee38c345
oshuakbaev <olzhason47@gmail.com>
2023-02-06 22:24:39 +0600

    flutter first app

---
+++ lab_works/flutter_first_widget/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.black),
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text("oshuakbayev flutter"),
+            centerTitle: true,
+        ), // шапка
+        body: Center(
+            child: Text("Hello My Flutter!", style:TextStyle(
+              fontSize: 50,
+              color: Colors.blueAccent,
+              fontFamily: 'Times New Roman',
+            ),)
+        ),
+      ),
+    );
+  }
+
+}
